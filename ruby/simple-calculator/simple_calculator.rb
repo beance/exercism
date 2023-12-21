f36973176ce7dc680522bb2b4f7c684874302563
Denis T <kiddypoker@gmail.com>
2023-12-21 23:33:05 +0300

    simple_calculator

---
+++ ruby/simple-calculator/simple_calculator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class SimpleCalculator
+  ALLOWED_OPERATIONS = %w[+ / *].freeze
+  class UnsupportedOperation < StandardError; end
+
+  def self.calculate(first_operand, second_operand, operation)
+    raise ArgumentError unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer)
+    raise UnsupportedOperation unless ALLOWED_OPERATIONS.include?(operation)
+
+    begin
+      "#{first_operand} #{operation} #{second_operand} = #{first_operand.send(operation, second_operand)}"
+    rescue ZeroDivisionError
+      'Division by zero is not allowed.'
+    end
+  end
+end
