18d6cc1b14c922faffa14e278481c5d8d1adb1b5
Denis T <kiddypoker@gmail.com>
2023-12-21 23:40:06 +0300

    moviegoer

---
+++ ruby/moviegoer/moviegoer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class NotMovieClubMemberError < RuntimeError
+end
+
+class Moviegoer
+  def initialize(age, member: false)
+    @age = age
+    @member = member
+  end
+
+  def ticket_price
+    @age >= 60 ? 10 : 15
+  end
+
+  def watch_scary_movie?
+    @age >= 18
+  end
+
+  def claim_free_popcorn!
+    @member ? '🍿' : (raise NotMovieClubMemberError)
+  end
+end
