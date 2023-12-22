a6b425a36e1e12ae9b14daec365e5808e111d127
Denis T <kiddypoker@gmail.com>
2023-12-22 12:06:51 +0300

    hamming

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Hamming
+  def self.compute(strand1, strand2)
+    raise ArgumentError if strand1.length != strand2.length
+
+    strand1.chars.zip(strand2.chars).count { |a, b| a != b }
+  end
+end
