fbe721c6e3c5743ebc18aa3781d4a20db4cbfe39
Denis T <kiddypoker@gmail.com>
2023-12-23 11:48:22 +0300

    twelve_days

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,7 +1,27 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+class TwelveDays
+  DAYS = {
+    1 => 'first',    2 => 'second', 3 => 'third', 4 => 'fourth', 5 => 'fifth',
+    6 => 'sixth',    7 => 'seventh', 8 => 'eighth',    9 => 'ninth', 10 => 'tenth',
+    11 => 'eleventh', 12 => 'twelfth'
+  }.freeze
+  GIFTS = [
+    'a Partridge in a Pear Tree', 'two Turtle Doves', 'three French Hens',
+    'four Calling Birds',  'five Gold Rings', 'six Geese-a-Laying',
+    'seven Swans-a-Swimming', 'eight Maids-a-Milking', 'nine Ladies Dancing',
+    'ten Lords-a-Leaping', 'eleven Pipers Piping', 'twelve Drummers Drumming'
+  ].freeze
+  def self.song
+    DAYS.values.map.with_index do |day, i|
+      "On the #{day} day of Christmas my true love gave to me: #{to_series(GIFTS.take(i + 1).reverse)}.\n"
+    end.join("\n")
+  end
+
+  def self.to_series(items)
+    *list, last_item = items
+    return last_item if items.one?
+
+    [*list, "and #{last_item}"].join(', ')
+  end
+end
