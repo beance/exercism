0487b9aef1b2e57cf353674af21d071695fb3114
Denis T <kiddypoker@gmail.com>
2023-12-25 11:31:51 +0300

    prime-factors

---
+++ ruby/prime-factors/prime_factors.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module PrimeFactors
+  def self.of(number)
+    return [] if number < 2
+
+    factor = (2..number).find { |i| (number % i).zero? }
+    [factor] + of(number / factor)
+  end
+end
