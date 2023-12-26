d4c3182e9e7c1cdbdbb601b2f89ce170c7a6449b
Denis T <kiddypoker@gmail.com>
2023-12-26 13:21:58 +0300

    robot-simulator

--- ruby/robot-simulator/robot_simulator.rb
+++ ruby/robot-simulator/robot_simulator.rb
@@ -1,7 +1,52 @@
-=begin
-Write your code for the 'Robot Simulator' exercise in this file. Make the tests in
-`robot_simulator_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/robot-simulator` directory.
-=end
+class Robot
+  BEARINGS = %i[north east south west].freeze
+  TURN_RIGHT = BEARINGS.zip(BEARINGS.rotate(1)).to_h.freeze
+  TURN_LEFT = BEARINGS.zip(BEARINGS.rotate(-1)).to_h.freeze
+  ADVANCE = BEARINGS.zip([[0, 1], [1, 0], [0, -1], [-1, 0]]).to_h.freeze
+  attr_reader :bearing, :coordinates
+
+  def initialize(bearing: :north, coordinates: [0, 0])
+    @bearing = bearing
+    @coordinates = coordinates
+  end
+
+  def orient(direction)
+    raise ArgumentError unless BEARINGS.include?(direction)
+
+    @bearing = direction
+  end
+
+  def turn_right
+    @bearing = TURN_RIGHT[@bearing]
+  end
+
+  def turn_left
+    @bearing = TURN_LEFT[@bearing]
+  end
+
+  def at(*coordinates)
+    @coordinates = coordinates.first(2)
+  end
+
+  def advance
+    @coordinates = @coordinates.zip(ADVANCE[@bearing]).map(&:sum)
+  end
+end
+
+class Simulator
+  INSTRUCTIONS = { 'R' => :turn_right, 'L' => :turn_left, 'A' => :advance }.freeze
+  def instructions(instructions)
+    instructions.chars.map(&INSTRUCTIONS.method(:[]))
+  end
+
+  def place(robot, x:, y:, direction:)
+    robot.at(x, y)
+    robot.orient(direction)
+  end
+
+  def evaluate(robot, instructions)
+    instructions(instructions).each(&robot.method(:send))
+  end
+end
