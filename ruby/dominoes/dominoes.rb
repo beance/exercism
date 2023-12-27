a40374e906a805e7ce672f758d6185d15a11f8ca
Denis T <kiddypoker@gmail.com>
2023-12-27 16:12:56 +0300

    dominoes

---
+++ ruby/dominoes/dominoes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Dominoes
+  def self.chain?(dominoes)
+    return true if dominoes.empty?
+
+    dominoes.permutation.any? do |part|
+      part.each_cons(2).all? { |a, b| a[1] == b[0] || a[1] == b.reverse![0] } && part[0][0] == part[-1][-1]
+    end
+  end
+end
